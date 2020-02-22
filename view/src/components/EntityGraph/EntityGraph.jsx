61b85cbe2e2f3bf9068773bd6905e6e9a14ecea2
David Lee <davkmlee@gmail.com>
2020-02-22 14:20:49 -0800

    implement force-directed layout of nodes

--- view/src/components/EntityGraph/EntityGraph.jsx
+++ view/src/components/EntityGraph/EntityGraph.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import {
+  forceSimulation,
+  forceManyBody,
+  forceLink,
+  forceCenter
+} from "d3-force";
 
 import { formatData } from "./graph-utils";
 
@@ -10,8 +16,8 @@ class EntityGraph extends Component {
 
     this.state = {
       allData: graph,
-      currNodes: graph.nodes,
-      currEdges: graph.edges,
+      currNodes: [],
+      currEdges: [],
       filter: null,
       layout: "force",
       width: 975,
@@ -21,8 +27,6 @@ class EntityGraph extends Component {
 
   initializeData(data) {
     data.nodes.forEach(node => {
-      node.cx = Math.floor(Math.random() * this.props.width);
-      node.cy = Math.floor(Math.random() * this.props.height);
       node.radius = 4;
     });
 
@@ -44,21 +48,37 @@ class EntityGraph extends Component {
     return nodesMap;
   }
 
+  componentDidMount() {
+    const currNodes = this.state.allData.nodes;
+    const currEdges = this.state.allData.edges;
+
+    const simulation = forceSimulation(currNodes)
+      .force("charge", forceManyBody().strength(-10))
+      .force("link", forceLink(currEdges))
+      .force("center", forceCenter());
+    simulation.on("tick", () => {
+      this.setState({ currNodes: currNodes });
+      this.setState({ currEdges: currEdges });
+    });
+  }
+
   render() {
     return (
       <svg
         className="graph"
         style={{ border: "2px solid #bcbcbc" }}
-        viewBox={`0 0 ${this.state.width} ${this.state.height}`}
+        viewBox={`${-this.state.width / 2} ${-this.state.height / 2} ${
+          this.state.width
+        } ${this.state.height}`}
       >
         <g id="edges">
           {this.state.currEdges.map(e => (
             <line
               className="edge"
-              x1={e.source.cx}
-              y1={e.source.cy}
-              x2={e.target.cx}
-              y2={e.target.cy}
+              x1={e.source.x}
+              y1={e.source.y}
+              x2={e.target.x}
+              y2={e.target.y}
               stroke="#ccc"
               strokeOpacity={0.8}
             ></line>
@@ -68,8 +88,8 @@ class EntityGraph extends Component {
           {this.state.currNodes.map(n => (
             <circle
               className="node"
-              cx={n.cx}
-              cy={n.cy}
+              cx={n.x}
+              cy={n.y}
               r={n.radius}
               style={{ strokeWidth: 1.0 }}
             ></circle>
