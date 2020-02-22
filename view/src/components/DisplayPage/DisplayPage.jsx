f47303e072cbc76225ad9973d8a2571c234d6944
David Lee <davkmlee@gmail.com>
2020-02-21 20:54:58 -0800

    prettier format displaypage component

--- view/src/components/DisplayPage/DisplayPage.jsx
+++ view/src/components/DisplayPage/DisplayPage.jsx
@@ -1,92 +1,87 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios'
-import Brat from '../Brat/Brat'
-import Graph from '../Graph/Graph'
-import { PacmanLoader } from 'react-spinners';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { addHighLight } from '../../utils';
-import './DisplayPage.css';
-import { getHost } from '../../utils';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Brat from "../Brat/Brat";
+import Graph from "../Graph/Graph";
+import { PacmanLoader } from "react-spinners";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { addHighLight } from "../../utils";
+import "./DisplayPage.css";
+import { getHost } from "../../utils";
 
 class DisplayPage extends Component {
-    state = {
-        docData: null,
+  state = {
+    docData: null
+  };
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    axios
+      .post(getHost() + ":3001/api/getCaseReportById", { id })
+      .then(res => {
+        const data = res.data.data[0];
+        this.setState({ docData: data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { id } = this.props.match.params;
+    const { docData } = this.state;
+    let text, // whole plain text of the case report
+      entities, // entities for graph
+      tokensToHighlight, // array of tokens to highlight
+      textEntities; // plain text highlight entities
+    if (docData) {
+      ({ text } = docData);
+    }
+    if (this.props.location.state) {
+      // console.log('data:', this.props.location.state);
+      ({ entities, textEntities } = this.props.location.state);
+      console.log(textEntities);
+      tokensToHighlight = textEntities.map(e => e.label);
+      console.log("tokensToHighlight:", tokensToHighlight);
     }
 
-    componentDidMount() {
-        const { id } = this.props.match.params;
-
-        axios.post(getHost() + ":3001/api/getCaseReportById", { id })
-            .then(res => {
-                const data = res.data.data[0];
-                this.setState({ docData: data })
-            })
-            .catch(err => console.log(err));
-    }
-
-
-    render() {
-        const { id } = this.props.match.params;
-        const { docData } = this.state;
-        let text,                   // whole plain text of the case report
-            entities,               // entities for graph
-            tokensToHighlight,      // array of tokens to highlight
-            textEntities;           // plain text highlight entities
-        if (docData) { ({ text } = docData); }
-        if (this.props.location.state) {
-            // console.log('data:', this.props.location.state);
-            ({ entities, textEntities } = this.props.location.state);
-            console.log(textEntities);
-            tokensToHighlight = textEntities.map(e => e.label);
-            console.log('tokensToHighlight:', tokensToHighlight);
+    return (
+      <div className="display-page">
+        <div className="brat-intro">
+          <FontAwesomeIcon icon={["fal", "file-alt"]} />
+          Details about case report <span className="report-id">{id}</span>
+        </div>
+        {docData && (
+          <div
+            className="report-plain-text"
+            dangerouslySetInnerHTML={{
+              __html: addHighLight(text, tokensToHighlight)
+            }}
+          />
+        )}
+        {docData && (
+          <div className="brat-container">
+            <Brat docData={docData} />
+          </div>
+        )}
+        {docData && (
+          <div className="graph-container">
+            <Graph graphData={docData} entities={entities} />
+          </div>
+        )}
         }
-
-        return (
-            <div className='display-page'>
-
-                <div className='brat-intro'>
-                    <FontAwesomeIcon icon={['fal', 'file-alt']} />
-                    Details about case report <span className='report-id'>{id}</span>
-                </div>
-
-                {docData &&
-                    <div
-                        className='report-plain-text'
-                        dangerouslySetInnerHTML={{
-                            __html: addHighLight(text, tokensToHighlight)
-                        }}
-                    />
-                }
-
-                {docData &&
-                    <div className='brat-container'>
-                        <Brat docData={docData} />
-                    </div>
-                }
-                {docData &&
-                    <div className='graph-container'>
-                        <Graph
-                            graphData={docData}
-                            entities={entities}
-                        />
-                    </div>
-                }}
-
-                {!docData &&
-                    <div className='loading-container'>
-                        {`Loading ......`}
-                        <PacmanLoader
-                            sizeUnit={"px"}
-                            size={150}
-                            color={'rgb(1, 136, 203)'}
-                        />
-                    </div>
-                }
-            </div>
-        );
-    }
+        {!docData && (
+          <div className="loading-container">
+            {`Loading ......`}
+            <PacmanLoader
+              sizeUnit={"px"}
+              size={150}
+              color={"rgb(1, 136, 203)"}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 // DisplayPage.propTypes = {
