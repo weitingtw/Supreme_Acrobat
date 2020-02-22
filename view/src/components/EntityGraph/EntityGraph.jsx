3bc123d0627f9a267d43efec6ec3eadeb706fe99
David Lee <davkmlee@gmail.com>
2020-02-21 23:27:17 -0800

    Set up skeleton of graph component

---
+++ view/src/components/EntityGraph/EntityGraph.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+import { formatData } from "./graph-utils";
+
+class EntityGraph extends Component {
+  state = {};
+  render() {
+    return null;
+  }
+}
+
+export default EntityGraph;
