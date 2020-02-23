29c6ba3ce42c71f528cda52c0fe1437572c1fca5
David Lee <davkmlee@gmail.com>
2020-02-22 20:33:26 -0800

    Add color to nodes

--- view/src/components/EntityGraph/EntityGraph.jsx
+++ view/src/components/EntityGraph/EntityGraph.jsx
@@ -17,12 +17,65 @@ class EntityGraph extends Component {
 
     const graph = this.initializeData(formatData(this.props.graphData));
 
+    const nodeColors = {
+      Age: "#EDC1F0", // Entities
+      Sex: "#EDC1F0",
+      Personal_background: "#EDC1F0",
+      Occupation: "#EDC1F0",
+      Weigh: "#EDC1F0",
+      Height: "#EDC1F0",
+      History: "ellipse",
+      Family_history: "#EDC1F0",
+      Family_member: "#EDC1F0",
+      Medication: "#2FCACA",
+      Lab: "#8f97ff",
+      Therapeutic_procedure: "#6495ed",
+      Diagnostic_procedure: "#9fdfff",
+      Sign_disease: "#f4eded",
+      Sign_symptom: "#DAE48B",
+      Disease_disorder: "#EB8315",
+      Activity: "#E07BAF",
+      Clinical_event: "#E07BAF",
+      Outcome: "#E07BAF",
+      Subject: "#ffd700",
+      Negation: "#ffd700",
+      Uncertainty: "#ffd700",
+      Condition: "#ffd700",
+      Quantitative_concept: "#ffd700",
+      Qualitative_concept: "#ffd700",
+      Other_entity: "#c1cdcd",
+      Other_event: "#c1cdcd",
+      Administration: "#ffd700",
+      Dosage: "#ffd700",
+      Frequency: "#ffd700",
+      Cause: "#ffd700",
+      Complication: "#ffd700",
+      Severity: "#ffd700",
+      Location: "#ffd700",
+      Result_outcome: "#ffd700",
+      Lab_value: "#A04AF0",
+      Biological_structure: "#ffd700",
+      Detail_description: "#ffd700",
+      Biological_attribute: "#ffd700",
+      Nonbiological_location: "#ffd700",
+      Detailed_description: "#ffd700",
+      Distance: "#ffd700",
+      Area: "#ffd700",
+      Volume: "#ffd700",
+      Mass: "#ffd700",
+      Color: "#ffd700",
+      Shape: "#ffd700",
+      Texture: "#ffd700",
+      Coreference: "#808000"
+    };
+
     this.state = {
       allData: graph,
       currNodes: graph.nodes,
       currEdges: graph.edges,
       filter: null,
-      layout: "force"
+      layout: "force",
+      colors: nodeColors
     };
   }
 
@@ -124,6 +177,7 @@ class EntityGraph extends Component {
               cy={n.y}
               r={n.radius}
               style={{ strokeWidth: 1.0 }}
+              fill={this.state.colors[n.type]}
             ></circle>
           ))}
         </g>
