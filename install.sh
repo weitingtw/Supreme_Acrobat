ede0c221d4cf9ff32743eee868d19273f10ba474
Wei-Ting Chen <weitingchen310@hotmail.com>
2020-02-23 23:17:18 -0800

    update install instructions

--- install.sh
+++ install.sh
@@ -34,6 +34,25 @@ cd ..
 npm install
 
 
+echo "                                                                                                                       
+-------------------------------------                                                                                        
+฿ retrieving ML Models ...                                                                                        
+-------------------------------------"
+
+fileid="1kQExz-YngdZU8OhRnOhEstlEP2vbGvQZ"
+filename="best-model.pt"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+
+fileid="1rcPqiyJUuss59VN5H0BN9dUWkukW6Y4B"
+filename="glove_model.joblib"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" 
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+rm cookie
+mv best-model.pt ML-API/best-model.pt
+mv glove_model.joblib ML-API/glove_model.joblib
 
 echo "
 ------------------------------------
