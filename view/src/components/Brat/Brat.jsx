89b3b82248fd750cd0330b798559bee1cd02acb0
David Lee <davkmlee@gmail.com>
2020-12-07 20:49:38 -0800

    format prettier

--- view/src/components/Brat/Brat.jsx
+++ view/src/components/Brat/Brat.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-import { Layout, Button, Modal, Row, Input } from 'antd';
-import {ExclamationCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { Layout, Button, Modal, Row, Input } from "antd";
+import {
+  ExclamationCircleOutlined,
+  CloseCircleOutlined,
+} from "@ant-design/icons";
 
-import { getHost } from '../../utils';
+import { getHost } from "../../utils";
 
 import PropTypes from "prop-types";
 import "./Brat.css";
-
 
 import collData from "./collData";
 // import defaultDocData from './defaultDocData';
@@ -30,7 +32,6 @@ class Brat extends Component {
   state = {
     docData: this.props.docData,
     confirmation_visible: false,
-
   };
   collData = collData;
 
@@ -48,44 +49,42 @@ class Brat extends Component {
     // console.log(this.state.docData);
     const data = this.state.docData;
 
-    axios.post(getHost() + "/api/putPendingCaseReport", data)
-    .then(res => {
-        console.log(res.data);
-        if (res.data.success === false) {
-            alert('upload failed')
-        } else {
-            alert('upload succeeded')
-            this.closeModal();
-        }
-    })
+    axios.post(getHost() + "/api/putPendingCaseReport", data).then((res) => {
+      console.log(res.data);
+      if (res.data.success === false) {
+        alert("upload failed");
+      } else {
+        alert("upload succeeded");
+        this.closeModal();
+      }
+    });
     // this should be the same? docData was passed by reference to brat!!
     // console.log(docData);
   };
 
-    redraw = () => {
-      if (window.brat != null) {
-        window.brat.dispatcher.post("requestRenderData", [this.state.docData]);
-        window.brat.dispatcher.post("current", [
-          this.collData,
-          this.state.docData,
-          {},
-        ]);
-      }
-    };
+  redraw = () => {
+    if (window.brat != null) {
+      window.brat.dispatcher.post("requestRenderData", [this.state.docData]);
+      window.brat.dispatcher.post("current", [
+        this.collData,
+        this.state.docData,
+        {},
+      ]);
+    }
+  };
 
-    openModal = () => {
-      this.setState({ confirmation_visible: true });
-    }
-  
-    closeModal = () => {
-      this.setState({ confirmation_visible: false });
-  
-    }
+  openModal = () => {
+    this.setState({ confirmation_visible: true });
+  };
+
+  closeModal = () => {
+    this.setState({ confirmation_visible: false });
+  };
 
   render() {
     return (
       <div className="brat">
-        <div className="button-container" >
+        <div className="button-container">
           <Button onClick={this.openModal}>Submit Changes</Button>
         </div>
         <div id="brat-editor" />
@@ -94,28 +93,26 @@ class Brat extends Component {
           redraw
         </span> */}
         <Modal
-                          visible={this.state.confirmation_visible}
-                          onOk={this.handleSubmit}
-                          onCancel={this.closeModal}
-                          footer={null}
-                          closeIcon={<CloseCircleOutlined />}
-                          destroyOnClose={false}
-          
+          visible={this.state.confirmation_visible}
+          onOk={this.handleSubmit}
+          onCancel={this.closeModal}
+          footer={null}
+          closeIcon={<CloseCircleOutlined />}
+          destroyOnClose={false}
         >
-          <p className='modal-header'>Confirm</p>
+          <p className="modal-header">Confirm</p>
           <p>Are you sure you want to submit the changes for review?</p>
-          <div className='modal-button-container'>
+          <div className="modal-button-container">
             <Button onClick={this.closeModal}>No</Button>
-            <Button type="primary" onClick={this.handleSubmit}>Yes</Button>
+            <Button type="primary" onClick={this.handleSubmit}>
+              Yes
+            </Button>
           </div>
-
         </Modal>
-
       </div>
     );
   }
 }
-
 
 Brat.propTypes = {
   docData: PropTypes.object.isRequired,
